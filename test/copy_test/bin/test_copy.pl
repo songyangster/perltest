73434e7f59b5862d70177f1e55ffba2829aa57a3
Song yang <songyang@yahoo-inc.com>
2017-07-11 15:19:40 -0700

    Test code to copy one directory to another

---
+++ test/copy_test/bin/test_copy.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl -w
+#
+use strict;
+#
+use File::Find;
+use File::Copy "cp";
+
+@ARGV==2 or die ("Not enough args: @ARGV");
+my ($input_dir, $output_dir) = @ARGV;
+
+sub process_file
+{
+    return 0 if (-d $_);
+
+    cp ($File::Find::name, $output_dir) or print ("Failed to copy $File::Find::name to $output_dir\n");
+    return 1;
+}
+
+find(\&process_file, $input_dir);
